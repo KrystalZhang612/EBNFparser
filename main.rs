636d99e5bf9ecb3e917517df8ca62e87eb54ae19
Krystal Zhang <72481348+KrystalZhang612@users.noreply.github.com>
2022-06-15 17:26:52 -0700

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,26 +1,26 @@
 use std::env;
-mod cstream_stage1;
-mod parser_stage3; 
-mod token_stage2; 
-mod scanner_stage2; 
+mod stage1;
+mod stage3; 
+mod stage2_token; 
+mod stage2_scanner; 
 mod xhtml_out; 
 
 fn main() {
 
 	env::set_var("RUST_BACKTRACE", "1");
 	//Testing Stage 1: 
-	cstream_stage1::CStreamTest(); 
+	stage1::CStreamTest(); 
 	//should print "Stage 1 successfully approached! All CStream tests passed!"
 	//Stage 1 approached. 
 	
 	//Testing Stage 2:
-	scanner_stage2::ScannerTest(); 
+	stage2_scanner::ScannerTest(); 
 	//should print "Stage 2 successfully approached! All characters in example1.x file tests passed!"
 	//Stage 2 approached 
 	
 	//Testing Stage 3:
-	parser_stage3::test(); 
-	parser_stage3::test_run(); 
+	stage3::test(); 
+	stage3::test_run(); 
 	
 	//outputting xhtml file
 	xhtml_out::test_run();
