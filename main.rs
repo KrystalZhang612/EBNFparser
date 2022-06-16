c7aa421ef07df29833b33ba4902ddf2ba21abe85
Krystal Zhang <72481348+KrystalZhang612@users.noreply.github.com>
2022-06-15 17:25:28 -0700

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,13 +1,9 @@
 use std::env;
 mod cstream_stage1;
-//mod parser_stage3; 
+mod parser_stage3; 
 mod token_stage2; 
 mod scanner_stage2; 
-//mod xhtml_out; 
-
-
-
-
+mod xhtml_out; 
 
 fn main() {
 
@@ -15,7 +11,7 @@ fn main() {
 	//Testing Stage 1: 
 	cstream_stage1::CStreamTest(); 
 	//should print "Stage 1 successfully approached! All CStream tests passed!"
-	//Stage 1 approached
+	//Stage 1 approached. 
 	
 	//Testing Stage 2:
 	scanner_stage2::ScannerTest(); 
@@ -23,14 +19,11 @@ fn main() {
 	//Stage 2 approached 
 	
 	//Testing Stage 3:
-	//parser_stage3::test(); 
-	//parser_stage3::test_run(); 
-	//error[E0463]: can't find crate for `custom_error`
-	//but the custom_error has always been in the dependencies in Cargo.toml
+	parser_stage3::test(); 
+	parser_stage3::test_run(); 
 	
 	//outputting xhtml file
-	//xhtml_out::test_run();
-	//since stage3 parser failed, stage4 output xhtml failed.
-	
+	xhtml_out::test_run();
+	//should be converting .x file into .xhtml file.	
 	
 }
